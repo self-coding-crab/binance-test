7c156ba849393af4609732680ce8f14f39a59067
iMac <imac@iMacs-iMac.local>
2020-01-21 09:36:16 +0700

    Backgound color update

--- src/modules/OrderBook/OrdersList.js
+++ src/modules/OrderBook/OrdersList.js
@@ -7,10 +7,17 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Typography from "@material-ui/core/Typography";
-import { Tooltip } from '@material-ui/core';
-import {FormattedMessage} from 'react-intl';
+import styled from 'styled-components';
 
-import { ToolTipWrapper } from '../../components/ToolTip'
+import { PAIRS_LIST } from '../../shared/constants/binance'
+
+const StyledRow = styled(TableRow)`
+    background: -webkit-linear-gradient(left, #303030 ${props => props.oWidth}%, #FF66CC ${props => props.width}%);
+    // background: -webkit-linear-gradient(left, black 97%, white 3%);
+
+    // background: -webkit-linear-gradient(right, #FF66CC ${props => props.width}%, #303030 ${props => 100 - props.width}%);
+    `
+
 const useStyles = makeStyles({
     root: {
         width: '100%',
@@ -28,8 +35,12 @@ const useStyles = makeStyles({
         width: 150
     },
 });
-export default function OrdersList({title, items}) {
+export default function OrdersList({title, items, pair}) {
     const classes = useStyles();
+    let sum = 0;
+    const standardBTC = 2.5;
+    const standardETH = 8
+    const standardLTC = 4
     return (
         <Paper className={classes.root}>
             <Typography variant="h4" gutterBottom className={classes.title}>
@@ -39,32 +50,33 @@ export default function OrdersList({title, items}) {
             <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell className={classes.cellPrice}>
-                            <FormattedMessage id="price" />
-                        </TableCell>
-                        <TableCell  className={classes.cellAmount}>
-                            <FormattedMessage id="amount" />
-                        </TableCell>
-                        <TableCell>
-                            <FormattedMessage id="total" />
-                        </TableCell>
+                        <TableCell className={classes.cellPrice}>Price</TableCell>
+                        <TableCell  className={classes.cellAmount}>Amount</TableCell>
+                        <TableCell>Total</TableCell>
+                        <TableCell>Sum</TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
                     {items.map((item, key) => {
                         const price = parseFloat(item[0]);
                         const amount = parseFloat(item[1]);
-                        const avgPrice = items.slice(key).reduce((a, b) => a + parseFloat(b[0]), 0)
-                        const btcSum = items.slice(key).reduce((a, b) => a + parseFloat(b[1]), 0)
-                        const total = items.slice(key).reduce((a, b) => a + parseFloat(b[0]) * parseFloat(b[1]), 0);
+                        const total = price * amount;
+                        sum += total;
+                        let width = 0;
+                        if (pair === PAIRS_LIST.btcusdt)
+                            width = amount / standardBTC
+                        else if (pair === PAIRS_LIST.ethusdt)
+                            width = amount / standardETH
+                        else
+                            width = amount / standardLTC
+                        width = parseInt(width * 100);
                         return (
-                            <Tooltip title={<ToolTipWrapper avgPrice={avgPrice.toFixed(2)} btcSum={btcSum.toFixed(6)} usdSum={total.toFixed(8)} />} key={key}>
-                                <TableRow key={key}>
-                                    <TableCell>{price.toFixed(2)}</TableCell>
-                                    <TableCell>{amount.toFixed(6) }</TableCell>
-                                    <TableCell>{total.toFixed(8)}</TableCell>
-                                </TableRow>
-                            </Tooltip>
+                            <StyledRow key={key} width={width} oWidth={100 - width}>
+                                <TableCell>{price.toFixed(2)}</TableCell>
+                                <TableCell>{amount.toFixed(6) }</TableCell>
+                                <TableCell>{total.toFixed(8)}</TableCell>
+                                <TableCell>{sum.toFixed(8)}</TableCell>
+                            </StyledRow>
                         )
                     })}
                 </TableBody>
