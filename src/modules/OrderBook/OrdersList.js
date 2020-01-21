509d19c2a0bb67b2c0a7a669bcae73022930ff58
iMac <imac@iMacs-iMac.local>
2020-01-21 16:25:34 +0700

    add missing part

--- src/modules/OrderBook/OrdersList.js
+++ src/modules/OrderBook/OrdersList.js
@@ -8,11 +8,14 @@ import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Typography from "@material-ui/core/Typography";
 import styled from 'styled-components';
+import { FormattedMessage } from 'react-intl';
+import { Tooltip } from '@material-ui/core';
 
 import { PAIRS_LIST } from '../../shared/constants/binance'
+import { ToolTipWrapper } from '../../components/ToolTip'
 
 const StyledRow = styled(TableRow)`
-    background: -webkit-linear-gradient(left, #303030 ${props => props.oWidth}%, #FF66CC ${props => props.width}%);
+    background: -webkit-linear-gradient(left, rgba(255,255,255,0) ${props => props.oWidth}%, #FF66CC ${props => props.width}%);
     // background: -webkit-linear-gradient(left, black 97%, white 3%);
 
     // background: -webkit-linear-gradient(right, #FF66CC ${props => props.width}%, #303030 ${props => 100 - props.width}%);
@@ -50,10 +53,15 @@ export default function OrdersList({title, items, pair}) {
             <Table aria-label="simple table">
                 <TableHead>
                     <TableRow>
-                        <TableCell className={classes.cellPrice}>Price</TableCell>
-                        <TableCell  className={classes.cellAmount}>Amount</TableCell>
-                        <TableCell>Total</TableCell>
-                        <TableCell>Sum</TableCell>
+                        <TableCell className={classes.cellPrice}>
+                            <FormattedMessage id="price" />
+                        </TableCell>
+                        <TableCell  className={classes.cellAmount}>
+                            <FormattedMessage id="amount" />
+                        </TableCell>
+                        <TableCell>
+                            <FormattedMessage id="total" />
+                        </TableCell>
                     </TableRow>
                 </TableHead>
                 <TableBody>
@@ -62,6 +70,11 @@ export default function OrdersList({title, items, pair}) {
                         const amount = parseFloat(item[1]);
                         const total = price * amount;
                         sum += total;
+
+                        const avgPrice = items.slice(key).reduce((a, b) => a + parseFloat(b[0]), 0)
+                        const btcSum = items.slice(key).reduce((a, b) => a + parseFloat(b[1]), 0)
+                        const usdSum = items.slice(key).reduce((a, b) => a + parseFloat(b[0]) * parseFloat(b[1]), 0);
+
                         let width = 0;
                         if (pair === PAIRS_LIST.btcusdt)
                             width = amount / standardBTC
@@ -71,12 +84,13 @@ export default function OrdersList({title, items, pair}) {
                             width = amount / standardLTC
                         width = parseInt(width * 100);
                         return (
-                            <StyledRow key={key} width={width} oWidth={100 - width}>
-                                <TableCell>{price.toFixed(2)}</TableCell>
-                                <TableCell>{amount.toFixed(6) }</TableCell>
-                                <TableCell>{total.toFixed(8)}</TableCell>
-                                <TableCell>{sum.toFixed(8)}</TableCell>
-                            </StyledRow>
+                            <Tooltip title={<ToolTipWrapper avgPrice={avgPrice.toFixed(2)} btcSum={btcSum.toFixed(6)} usdSum={usdSum.toFixed(8)} />} key={key}>
+                                <StyledRow key={key} width={width} oWidth={100 - width}>
+                                    <TableCell>{price.toFixed(2)}</TableCell>
+                                    <TableCell>{amount.toFixed(6) }</TableCell>
+                                    <TableCell>{total.toFixed(8)}</TableCell>
+                                </StyledRow>
+                            </Tooltip>
                         )
                     })}
                 </TableBody>
